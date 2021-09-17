0537530bd9698a6c727a3f8e4b113f86fe95f892
Manuela Briere <manuelatgkb@gmail.com>
2021-09-17 14:48:37 -0300

    laçco com while com invremento

--- src/modulo1/aula6/a6_2_semanal.java
+++ src/modulo1/aula6/a6_2_semanal.java
@@ -3,10 +3,22 @@ package modulo1.aula6;
 public class a6_2_semanal {
     public static void main(String[] args){
 
-        int contador =0;
+        /*int contador =0;
         while(contador<5){
             System.out.println(contador);
             contador++;
+        }
+        System.out.printf("O valor final: %d", contador);*/
+
+        int num = 1;
+
+        while (num <= 10){
+            int mult = 0;
+            while (mult<=10){
+                System.out.println(num*mult);
+                mult++;
+            }
+            num++;
         }
     }
 }
