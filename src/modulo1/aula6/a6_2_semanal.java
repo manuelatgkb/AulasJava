d1fd9a67840a75e7f53c2214986149443bdd2f4b
Manuela Briere <manuelatgkb@gmail.com>
2021-09-17 14:28:49 -0300

    lasso1

---
+++ src/modulo1/aula6/a6_2_semanal.java
@@ -0,0 +1,12 @@
+package modulo1.aula6;
+
+public class a6_2_semanal {
+    public static void main(String[] args){
+
+        int contador =0;
+        while(contador<5){
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
